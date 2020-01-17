9cb6cb2ec6476dd17ac31e6fdff95764925a1b9d
nrsha <nrsha190@gmail.com>
2020-01-17 11:01:42 +0000

    changing pen depth

--- animate.m
+++ animate.m
@@ -2,13 +2,35 @@ close all;
 clearvars -except df mean_intensity
 % rgb choice:
 rgb = 3;
+c = ['r','g','b'];
 % folder choice (order by name in parent dir)
-folder = 2;
+folder = 1;
 % get a long figure window (to max size of image)
 figure('Renderer', 'painters', 'Position', [500 500 1600 500])
-
+threshold = 'n';
 for n = 1:16    
-    % plotting image
+
+    % find pen depth    
+    y = mean_intensity{1,folder}(:,n,rgb);
+    x = 1:length(y);
+    if length(y) > 500
+        [A, i1] = max(diff(y(1:500)));
+    else
+        [A, i1] = max(diff(y));
+    end
+
+    if threshold == 'y'&& sum(y > 0) > length(y)*0.90 
+        x = x(y>y(1));
+        y = y(y>y(1));
+        i2 = length(x);    
+    else
+%        [~, i2] = min(y);         % min distance
+%        [~, i2] = min(y(i1:end)); % min distance from well
+        
+        i2 = max(x(y > floor(max(y)/2) & (y < ceil(max(y)/2))));
+
+    end
+    
     subplot(3,1,1)
     channel = df{1,folder}{3*(n- 1) + rgb,1};
     channel_name = df{1,folder}{3*(n- 1)+ rgb,2};
@@ -18,9 +40,12 @@ for n = 1:16
         threshold = 70;
     end
     imshow(channel, [0 threshold]);
+    yL = get(gca,'YLim');
+    hold on;
+    line([x(i2) x(i2)],yL,'LineWidth', 3, 'LineStyle', ':', 'Color', c(rgb));
     % plotting average intensity
     subplot(3,1,2);
-    plot(mean_intensity{1,folder}(:,n,rgb));
+    plot(mean_intensity{1,folder}(:,n,rgb), 'Color',c(rgb));
     hold on;
     plot(mean_intensity{1,folder}(:,n,rgb) + std(double(df{1,folder}{3*(n- 1) + rgb,1}))', '--r');
     plot(mean_intensity{1,folder}(:,n,rgb) - std(double(df{1,folder}{3*(n- 1) + rgb,1}))', '--r');
@@ -29,4 +54,13 @@ for n = 1:16
     ylim([0, threshold])
     title(channel_name);
 
+    subplot(3,1,3);
+    
+    plot(x, y,'LineWidth', 1, 'Color', c(rgb));
+    hold on
+    yL = get(gca,'YLim');
+    line([x(i1) x(i1)],yL,'LineWidth', 2, 'LineStyle', '--', 'Color', 'k');
+    line([x(i2) x(i2)],yL,'LineWidth', 1.5, 'LineStyle', ':', 'Color', 'k');
+    
+    pause(0.2)
 end
