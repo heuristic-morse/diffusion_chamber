e5a8972630eda8a5fc67ea6916b1e20833902857
nrsha <heuristic_morse@gmail.com>
2020-01-27 16:44:05 +0000

    now with giffing

--- animate.m
+++ animate.m
@@ -5,7 +5,9 @@ c = ['r','g','b'];
 % folder choice (order by name in parent dir)
 folder = 1;
 % get a long figure window (to max size of image)
-figure('Renderer', 'painters', 'Position', [500 500 1600 500])
+fig_gif = figure('Renderer', 'painters', 'Position', [500 500 1600 500]);
+axis tight manual % this ensures that getframe() returns a consistent size
+filename = 'testAnimated.gif';
 threshold = 'n';
 for n = 1:16    
 
@@ -28,7 +30,7 @@ for n = 1:16
 
     end
     
-    subplot(3,1,1)
+    subplot(2,1,1)
     channel = df{1,folder}{3*(n- 1) + rgb,1};
     channel_name = df{1,folder}{3*(n- 1)+ rgb,2};
     if rgb == 1
@@ -41,24 +43,36 @@ for n = 1:16
     hold on;
     line([x(i2) x(i2)],yL,'LineWidth', 3, 'LineStyle', ':', 'Color', c(rgb));
     % plotting average intensity
-    subplot(3,1,2);
-    plot(mean_intensity{1,folder}(:,n,rgb), 'Color',c(rgb));
-    hold on;
-    plot(mean_intensity{1,folder}(:,n,rgb) + std(double(df{1,folder}{3*(n- 1) + rgb,1}))', '--r');
-    plot(mean_intensity{1,folder}(:,n,rgb) - std(double(df{1,folder}{3*(n- 1) + rgb,1}))', '--r');
-    hold off;
-    % set the ylimits
-    ylim([0, threshold])
-    title(channel_name);
+%     subplot(3,1,2);
+%     plot(mean_intensity{1,folder}(:,n,rgb), 'Color',c(rgb));
+%     hold on;
+%     plot(mean_intensity{1,folder}(:,n,rgb) + std(double(df{1,folder}{3*(n- 1) + rgb,1}))', '--r');
+%     plot(mean_intensity{1,folder}(:,n,rgb) - std(double(df{1,folder}{3*(n- 1) + rgb,1}))', '--r');
+%     hold off;
+%     % set the ylimits
+%     ylim([0, threshold])
+%     title(channel_name);
 
-    subplot(3,1,3);
+    subplot(2,1,2);
     
     plot(x, y,'LineWidth', 1, 'Color', c(rgb));
     hold on
     yL = get(gca,'YLim');
     line([x(i1) x(i1)],yL,'LineWidth', 2, 'LineStyle', '--', 'Color', 'k');
     line([x(i2) x(i2)],yL,'LineWidth', 1.5, 'LineStyle', ':', 'Color', 'k');
-    concentration = trapz(y);
-    title(sprintf('c = %s', concentration));
-    pause()
+%     concentration = trapz(y);
+%     title(sprintf('c = %s', concentration));
+%    pause()
+    
+
+    % Capture the plot as an image 
+    frame = getframe(fig_gif); 
+    im = frame2im(frame); 
+    [imind,cm] = rgb2ind(im,256); 
+    % Write to the GIF File 
+    if n == 1 
+      imwrite(imind,cm,filename,'gif', 'Loopcount',inf); 
+    else 
+      imwrite(imind,cm,filename,'gif','WriteMode','append'); 
+    end 
 end
