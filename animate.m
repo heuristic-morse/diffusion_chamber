6d2999709e2322c20191e8563922884b8b49cd97
nrsha <heuristic_morse@gmail.com>
2020-02-05 16:35:48 +0000

    additional changes

--- animate.m
+++ animate.m
@@ -1,6 +1,6 @@
 close all;
 % rgb choice:
-rgb = 3;
+rgb = 2;
 c = ['r','g','b'];
 % folder choice (order by name in parent dir)
 folder = 1;
@@ -8,7 +8,6 @@ folder = 1;
 fig_gif = figure('Renderer', 'painters', 'Position', [500 500 1600 500]);
 axis tight manual % this ensures that getframe() returns a consistent size
 filename = 'testAnimated.gif';
-threshold = 'n';
 [~, N, ~] =  size(mean_intensity{1,1});
 for n = 1:N    
 
@@ -16,17 +15,15 @@ for n = 1:N
     i1 = get_well_posn(mean_intensity{1,folder}, 'y');
 
     y = mean_intensity{1,folder}(:,n,rgb);
-    y = y - min(y);
     x = 1:length(y);
-
-    if threshold == 'y'&& sum(y > 0) > length(y)*0.90 
-        x = x(y>y(1));
-        y = y(y>y(1));
-        i2 = length(x);    
-    else
-        h=0.2;
-        i2 = max(x((y > floor(max(y)*h)) & (y < ceil(max(y)*h))));
-    end
+    
+    xx = x;
+    yy = y;
+    
+    y = y(x>i1*1.1) - min(y(x>i1*1.1));
+    
+    h=0.5;
+    i2 = min(x((y > floor(max(y)*h)) & (y < ceil(max(y)*h))));
     
     subplot(2,1,1)
     channel = df{1,folder}{3*(n- 1) + rgb,1};
@@ -53,7 +50,7 @@ for n = 1:N
 
     subplot(2,1,2);
     
-    plot(x, y,'LineWidth', 1, 'Color', c(rgb));
+    plot(xx, yy,'LineWidth', 1, 'Color', c(rgb));
     hold on
     yL = get(gca,'YLim');
     line([i1, i1],yL,'LineWidth', 2, 'LineStyle', '--', 'Color', 'k');
@@ -61,7 +58,6 @@ for n = 1:N
 %     concentration = trapz(y);
 %     title(sprintf('c = %s', concentration));
 %    pause()
-    
 
     % Capture the plot as an image 
     frame = getframe(fig_gif); 
