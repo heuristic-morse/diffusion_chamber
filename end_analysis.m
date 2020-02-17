964d8c34596d54e1b3edc17380f78febf54069d3
nrsha <heuristic_morse@gmail.com>
2020-02-17 12:02:58 +0000

    wokring triplicate

--- end_analysis.m
+++ end_analysis.m
@@ -1,22 +1,20 @@
 
 %% Calculate percentage change over all experiments
-
 colors = ['r', 'g', 'b'];
 %'y' for % change, else, penetration depth in units
 pc_opt = 'y';
 
 % get data for bar charts
 exp_results = [];
-exp_labels = [];
-c_opt = [2 3];
+exp_labels = {};
+c_opt = [1 3];
 for i = 1:length(pen_depth)
     if pc_opt == 'y'
        exp_results = horzcat(exp_results,pct_change{i}(end, c_opt));
     else
        exp_results = horzcat(exp_results,pen_depth{i}(end, c_opt));
     end
-    label =  horzcat(repmat([df{1,i}{1,2}(1:5),'  '],2,1), colors(c_opt)');
-    exp_labels = vertcat(exp_labels,string(label));
+    exp_labels{i} = horzcat(repmat([df{1,i}{1,2}(1:5),'  '],length(c_opt),1), colors(c_opt)');
 end
 
 %get colours for bar charts
@@ -28,29 +26,36 @@ end
 %plot colours for all experiments
 figure('Renderer', 'painters', 'Position', [500 500 800 1600]);
 m = 1;
+ax = gca;
 hb = bar(exp_results);
 hb.FaceColor = 'flat';
-set(gca,'xticklabel',exp_labels)
+ax.XTickLabel = exp_labels;
 hb.CData(:,:) = repmat(base, length(pen_depth),1);
 
-%TODO : add mean and std across colours
-% idx = 1;
-% for n = 1:N_gel
-%      for rgb = 2:3
-%         c_m = mean([pen_depth{3*N_g - 2}(end, rgb), ...
-%                     pen_depth{3*N_g - 1}(end,rgb), ...
-%                     pen_depth{3*N_g}(end,rgb)]);
-%         c_std = std([pen_depth{3*N_g - 2}(end, rgb), ...
-%                     pen_depth{3*N_g - 1}(end, rgb), ...
-%                     pen_depth{3*N_g}(end,rgb)]);
-%         rgb_stats(:, rgb) = [c_m; c_std];
-%         hb(rgb) = bar(idx, rgb_stats(1,rgb));
-%         hold all
-%         hb(rgb).FaceColor = colors(rgb);
-%         plt(rgb) = errorbar(idx, rgb_stats(1,rgb),rgb_stats(2,rgb), 'or', 'MarkerSize', 8);
-%         idx = idx + 1;
-%     end
-% end
+% getting triplicate data
+%number of independent exps (ie gels)
+n_gels = length(exp_results)/length(c_opt)/3;
+trp_mean = []; trp_std = [];trp_labels = {};
+for n = 1:n_gels
+    for c = 1:length(c_opt)
+        triplicates = linspace(6*(n-1) + c,6*(n-1)+ 4+c,3);
+        c_m(:,c) = mean(exp_results(triplicates));
+        c_std(:,c) = std(exp_results(triplicates));
+    end
+    trp_mean = horzcat(trp_mean, c_m);
+    trp_std = horzcat(trp_std, c_std);
+    trp_labels{n} = horzcat(exp_labels{3*(n-1) + 1}(:,1:end-2), colors(c_opt)');
+end
+
+%plot colours for trip experiments
+figure('Renderer', 'painters', 'Position', [500 500 800 1600]);
+ax = gca;
+hb = bar(trp_mean);
+hb.FaceColor = 'flat';
+hold on
+errorbar(1:length(trp_mean), trp_mean,trp_std, 'or', 'MarkerSize', 8);
+ax.XTickLabel = trp_labels;
+hb.CData(:,:) = repmat(base, length(trp_mean)/length(c_opt),1);
 
 %% Calculate change in concentration
 
