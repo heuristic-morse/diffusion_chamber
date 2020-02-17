09ce95e407cbd9c04c097a9f74ff6bb9d370f6c0
nrsha <heuristic_morse@gmail.com>
2020-02-17 10:55:59 +0000

    working bar

--- end_analysis.m
+++ end_analysis.m
@@ -1,65 +1,57 @@
 
 %% Calculate percentage change over all experiments
-N_g = 2;
-colors = ['r', 'g', 'b'];
-noise = 1;
-for i = 1:length(pen_depth)
-    if i > 1
-        noise = rand(1,3);
-    end
-    mean_pd(i,:) = pen_depth{i}(end, :).*noise;
-    %std_pd(i,:) = pen_depth{i}.*noise;
 
-    mean_pc(i,:) = pct_change{i}(end, :).*noise*100;
-    %std_pc(i,:) = std(pct_change{i}).*noise*100;
-end
-figure
-rgb_stats = [];
-m = 1;
-%TODO : generalise outside of triplicates, std, smarter way than idx
-idx = 1;
-for n = 1:N_gel
-     for rgb = 2:3
-        c_m = mean([pen_depth{3*N_g - 2}(end, rgb), ...
-                    pen_depth{3*N_g - 1}(end,rgb), ...
-                    pen_depth{3*N_g}(end,rgb)]);
-        c_std = std([pen_depth{3*N_g - 2}(end, rgb), ...
-                    pen_depth{3*N_g - 1}(end, rgb), ...
-                    pen_depth{3*N_g}(end,rgb)]);
-        rgb_stats(:, rgb) = [c_m; c_std];
-        hb(rgb) = bar(idx, rgb_stats(1,rgb));
-        hold all
-        hb(rgb).FaceColor = colors(rgb);
-        plt(rgb) = errorbar(idx, rgb_stats(1,rgb),rgb_stats(2,rgb), 'or', 'MarkerSize', 8);
-        idx = idx + 1;
+colors = ['r', 'g', 'b'];
+%'y' for % change, else, penetration depth in units
+pc_opt = 'y';
+
+% get data for bar charts
+exp_results = [];
+exp_labels = [];
+c_opt = [2 3];
+for i = 1:length(pen_depth)
+    if pc_opt == 'y'
+       exp_results = horzcat(exp_results,pct_change{i}(end, c_opt));
+    else
+       exp_results = horzcat(exp_results,pen_depth{i}(end, c_opt));
     end
+    label =  horzcat(repmat([df{1,i}{1,2}(1:5),'  '],2,1), colors(c_opt)');
+    exp_labels = vertcat(exp_labels,string(label));
 end
-% 
-% figure('Renderer', 'painters', 'Position', [500 500 800 1600]);
-% ymax = max(max(mean_pc));% + max(max(std_pc));
-% for rgb = 1:3
-% %    subplot(3,1,rgb)
-%     bar(1:length(mean_pd), mean_pd(:,rgb))
-%     hold all
-%  %   ylim([0 ymax]);
-%     xlim([0 length(pen_depth)+1]);
-%     xlabel('experiment number')
-%     ylabel('average penetration depth (%pct)')
-%     xticks(0:length(pen_depth))
+
+%get colours for bar charts
+base = repmat([0 0 0], length(c_opt),1);
+for n = 1:length(c_opt)
+    base(n, c_opt(n)) = 1;    
+end
+
+%plot colours for all experiments
+figure('Renderer', 'painters', 'Position', [500 500 800 1600]);
+m = 1;
+hb = bar(exp_results);
+hb.FaceColor = 'flat';
+set(gca,'xticklabel',exp_labels)
+hb.CData(:,:) = repmat(base, length(pen_depth),1);
+
+%TODO : add mean and std across colours
+% idx = 1;
+% for n = 1:N_gel
+%      for rgb = 2:3
+%         c_m = mean([pen_depth{3*N_g - 2}(end, rgb), ...
+%                     pen_depth{3*N_g - 1}(end,rgb), ...
+%                     pen_depth{3*N_g}(end,rgb)]);
+%         c_std = std([pen_depth{3*N_g - 2}(end, rgb), ...
+%                     pen_depth{3*N_g - 1}(end, rgb), ...
+%                     pen_depth{3*N_g}(end,rgb)]);
+%         rgb_stats(:, rgb) = [c_m; c_std];
+%         hb(rgb) = bar(idx, rgb_stats(1,rgb));
+%         hold all
+%         hb(rgb).FaceColor = colors(rgb);
+%         plt(rgb) = errorbar(idx, rgb_stats(1,rgb),rgb_stats(2,rgb), 'or', 'MarkerSize', 8);
+%         idx = idx + 1;
+%     end
 % end
-% 
-% figure('Renderer', 'painters', 'Position', [500 500 800 1600]);
-% ymax = max(max(mean_pd)); %+ max(max(std_pd));
-% for rgb = 1:3
-% %    subplot(3,1,rgb)
-%     bar(1:length(mean_pc), mean_pc(:,rgb))
-%     hold all
-%  %   ylim([0 ymax]);
-%     xlim([0 length(pen_depth)+1]);
-%     xlabel('experiment number')
-%     ylabel('average penetration depth (a.u units)')
-%     xticks(0:length(pen_depth))
-% end
+
 %% Calculate change in concentration
 
 figure('Renderer', 'painters', 'Position', [500 500 800 1600]);
